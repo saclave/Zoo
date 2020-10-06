0e8ba41f0e9fb3d0fb718f24b43026086c7c2f00
VEA MHAY JOY SACLAO (SHP-IRIS-ISD-OOCLL/MNL) <SACLAVE@oocl.com>
2020-10-06 11:30:51 +0800

    Added Zoo project for Java Inheritance exercise

---
+++ Zoo/src/Felidae.java
@@ -0,0 +1,7 @@
+public class Felidae extends Animal{
+    public String behavior = "Meow, meow";
+    public Felidae(int age, double weight, double running, String eating) {
+        super(age, weight, running, eating);
+    }
+}
+
